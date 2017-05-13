26c53ee2e7074ab19011314f740f7393691cb57c
Soham Chowdhury <chow.soham@gmail.com>
2017-05-13 23:03:23 +0530

    Add prelude module for type-level stuff

---
+++ library/Noether/Lemmata/TypeFu.hs
@@ -0,0 +1,17 @@
+module Noether.Lemmata.TypeFu
+  ( module X
+  ) where
+
+import           GHC.Exts           as X
+import           GHC.TypeLits       as X
+
+import           Data.Coerce        as X
+import           Data.Proxy         as X
+
+import           Data.Kind          as X hiding (type (*))
+import           Data.Type.Bool     as X
+import           Data.Type.Equality as X
+
+-- import           Data.Data          as X
+-- import           Data.Typeable      as X
+-- import           GHC.Generics       as X
