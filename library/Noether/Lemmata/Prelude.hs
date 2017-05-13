447502daf366bce60c3baf78f0a97fac45e414ba
Soham Chowdhury <chow.soham@gmail.com>
2017-05-13 23:03:58 +0530

    Add proto-Prelude ;)

---
+++ library/Noether/Lemmata/Prelude.hs
@@ -0,0 +1,18 @@
+module Noether.Lemmata.Prelude
+  ( module X
+  , fromInteger
+  , fromString
+  ) where
+
+import           Prelude     as X hiding (Eq, Monoid, fromInteger, negate,
+                                   recip, (*), (+), (-), (/), (==))
+
+import qualified Data.String as S
+import qualified Prelude     as P
+
+fromInteger :: Num a => Integer -> a
+fromInteger = P.fromInteger
+
+fromString :: S.IsString a => String -> a
+fromString = S.fromString
+
