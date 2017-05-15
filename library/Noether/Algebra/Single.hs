5cbf64de5aac83afd664206096bf16722dcd31ff
Soham Chowdhury <chow.soham@gmail.com>
2017-05-15 18:51:44 +0530

    Begin inference

--- library/Noether/Algebra/Single.hs
+++ library/Noether/Algebra/Single.hs
@@ -9,14 +9,14 @@ import           Noether.Lemmata.TypeFu
 import           Noether.Algebra.Single.Synonyms
 import           Noether.Algebra.Single.Types
 
-cancel :: forall op a . Cancellative op a => Proxy op -> a -> a
-cancel p = cancelK p (cancellativeSP @op @a)
+cancel :: forall op a. Cancellative op a => Proxy op -> a -> a
+cancel p = cancelK p (Proxy :: Proxy (CancellativeS op a))
 
-binaryOp :: forall op a . Magma op a => Proxy op -> a -> a -> a
-binaryOp p = binaryOpK p (magmaSP @op @a)
+binaryOp :: forall op a. Magma op a => Proxy op -> a -> a -> a
+binaryOp p = binaryOpK p (Proxy :: Proxy (MagmaS op a))
 
 neutral :: forall op a. Neutral op a => Proxy op -> a
-neutral p = neutralK p (neutralSP @op @a)
+neutral p = neutralK p (Proxy :: Proxy (NeutralS op a))
 
 zero :: Neutral Add a => a
 zero = neutral AddP
@@ -24,29 +24,29 @@ zero = neutral AddP
 one :: Neutral Mul a => a
 one = neutral MulP
 
-negate :: Abelian Add a => a -> a
+negate :: Cancellative Add a => a -> a
 negate = cancel AddP
 
-reciprocal :: Group Mul a => a -> a
+reciprocal :: Cancellative Mul a => a -> a
 reciprocal = cancel MulP
 
 infixl 6 +
 
-(+) :: Semigroup Add a => a -> a -> a
+(+) :: Magma Add a => a -> a -> a
 (+) = binaryOp AddP
 
 infixl 7 *
 
-(*) :: Semigroup Mul a => a -> a -> a
+(*) :: Magma Mul a => a -> a -> a
 (*) = binaryOp MulP
 
 infixl 6 -
 
-(-) :: Abelian Add a => a -> a -> a
+(-) :: (Cancellative Add a, Magma Add a) => a -> a -> a
 a - b = a + negate b
 
 infixl 7 /
 
-(/) :: Group Mul a => a -> a -> a
+(/) :: (Cancellative Mul a, Magma Mul a) => a -> a -> a
 a / b = a * reciprocal b
 
--- library/Noether/Algebra/Single/Types.hs
+++ library/Noether/Algebra/Single/Types.hs
@@ -10,108 +10,74 @@ import           Data.Complex
 import           Noether.Lemmata.Prelude
 import           Noether.Lemmata.TypeFu
 
-data UnaryTag = Neg
-  deriving Show
-
-data BinaryTag = Add | Mul
-  deriving Show
+data UnaryTag = Neg deriving Show
+data BinaryTag = Add | Mul deriving Show
 
 data Peano = Z | S Peano
 
-class UnaryNeutralK (op :: UnaryTag) a s where
+class UnaryNeutralK (op :: UnaryTag) a (s :: k') where
   unaryNeutralK :: Proxy op -> Proxy s -> a
 
-class UnaryOpK (op :: UnaryTag) a s where
+class UnaryOpK (op :: UnaryTag) a (s :: k') where
   unaryOpK :: Proxy op -> Proxy s -> a -> a
 
--- | Magma
-
-type family MagmaS (op :: k) (a :: Type) = (r :: Type)
-type Magma op a = MagmaK op a (MagmaS op a)
-
-class MagmaK (op :: k) a s where
-  binaryOpK :: Proxy op -> Proxy s -> a -> a -> a
-
--- | NeutralK
-
-type family NeutralS (op :: k) (a :: Type) = (r :: Type)
-type Neutral op a = (NeutralK op a (NeutralS op a))
-
-class NeutralK (op :: k) a s where
-  neutralK :: Proxy op -> Proxy s -> a
-
--- | Cancellative
-
-class CancellativeK op a s where
-  cancelK :: Proxy op -> Proxy s -> a -> a
-
-type family CancellativeS (op :: k) (a :: Type) = (r :: Type)
-
-type Cancellative (op :: k) a = CancellativeK op a (CancellativeS op a)
-
--- (FIXME: any dependencies?)
-
-type family CommutativeS (op :: k) (a :: Type) = (r :: Type)
-
--- FIXME: move constraint into instance, derive...?
-class (Magma op a) => CommutativeK (op :: k) a s
-
-type Commutative op a = (CommutativeK op a (CommutativeS op a))
-
-{-| Semigroup
-
- -}
-
-type family SemigroupS (op :: k) (a :: Type) = (r :: Type)
-
-class (Magma op a) => SemigroupK (op :: k) a s
-
-type Semigroup op a = (SemigroupK op a (SemigroupS op a))
-
-{-| Cancellative semigroups
-
- -}
-
-
+type family MagmaS                 (op :: k) (a :: Type) = (r :: Type)
+type family NeutralS               (op :: k) (a :: Type) = (r :: Type)
+type family CancellativeS          (op :: k) (a :: Type) = (r :: Type)
+type family CommutativeS           (op :: k) (a :: Type) = (r :: Type)
+type family SemigroupS             (op :: k) (a :: Type) = (r :: Type)
+type family MonoidS                (op :: k) (a :: Type) = (r :: Type)
+type family GroupS                 (op :: k) (a :: Type) = (r :: Type)
 type family CancellativeSemigroupS (op :: k) (a :: Type) = (r :: Type)
 
-class (Semigroup op a, Cancellative op a) =>
-      CancellativeSemigroupK (op :: k) a s
-
-type CancellativeSemigroup op a = CancellativeSemigroupK op a (CancellativeSemigroupS op a)
-
-
-{-| Monoids
-
--}
-
-type family MonoidS (op :: k) (a :: Type) = (r :: Type)
-
-class (Semigroup op a, Neutral op a) => MonoidK (op :: k) a s
-
+type Neutral op a = NeutralK op a (NeutralS op a)
+type Commutative op a = CommutativeK op a (CommutativeS op a)
+type Magma op a = MagmaK op a (MagmaS op a)
+type Semigroup op a = SemigroupK op a (SemigroupS op a)
+type Cancellative op a = CancellativeK op a (CancellativeS op a)
 type Monoid op a = MonoidK op a (MonoidS op a)
-
-{-| Groups
-
--}
-
-class (Monoid op a, Cancellative op a) => GroupK op a s
-
-type family GroupS (op :: k) (a :: Type) = (r :: Type)
-
 type Group op a = GroupK op a (GroupS op a)
 
-type CommSemigroup op a = (Commutative op a, Semigroup op a)
-type CommMonoid    op a = (Commutative op a, Monoid    op a)
-type Abelian       op a = (Commutative op a, Group     op a)
+class MagmaK (op :: k) a (s :: k') where
+  binaryOpK :: Proxy op -> Proxy s -> a -> a -> a
 
-data Composite (a :: k) (b :: k')
+class NeutralK (op :: k) a (s :: k') where
+  neutralK :: Proxy op -> Proxy s -> a
 
---
+class CancellativeK (op :: k) a (s :: k') where
+  cancelK :: Proxy op -> Proxy s -> a -> a
+
+class CommutativeK (op :: k) a (s :: k')
+
+class SemigroupK (op :: k) a (s :: k')
+
+class MonoidK (op :: k) a (s :: k')
+
+class GroupK (op :: k) a (s :: k')
+
+type CancellativeSemigroup op a = (Cancellative op a, Semigroup op a)
+type CommSemigroup         op a = (Commutative  op a,  Semigroup op a)
+type CommMonoid            op a = (Commutative  op a,  Monoid    op a)
+type Abelian               op a = (Commutative  op a,  Group     op a)
+
+data Composite (a :: [k])
+
+data (:=) (sym :: k) (t :: k')
+
+instance (SemigroupK op a zs, NeutralK op a zn) =>
+         MonoidK op a (Composite [SemigroupK := zs, NeutralK := zn])
+
+instance (MonoidK op a zm, CancellativeK op a zc) =>
+         GroupK op a (Composite [MonoidK := zm, CancellativeK := zc])
+
+instance SemigroupK   op a Prim
+instance CommutativeK op a Prim
 
 data DerivedFrom (a :: k)
 
 data Prim
+
+-- Double
 
 type instance MagmaS         (_ :: BinaryTag) Double = Prim
 type instance NeutralS       (_ :: BinaryTag) Double = Prim
@@ -119,30 +85,24 @@ type instance CancellativeS  (_ :: BinaryTag) Double = Prim
 type instance CommutativeS   (_ :: BinaryTag) Double = Prim
 
 type instance SemigroupS     (_ :: BinaryTag) Double = Prim
-type instance MonoidS        (_ :: BinaryTag) Double = Prim
-type instance GroupS         (_ :: BinaryTag) Double = Prim
 
--- Double, additive structure
+type instance MonoidS t Double =
+     Composite
+       [SemigroupK := SemigroupS t Double
+       , NeutralK := NeutralS t Double]
+
+type instance GroupS t Double =
+     Composite
+       [MonoidK := MonoidS t Double,
+        CancellativeK := CancellativeS t Double]
 
 instance MagmaK         Add Double Prim where binaryOpK _ _ = (P.+)
 instance NeutralK       Add Double Prim where neutralK  _ _ = 0
 instance CancellativeK  Add Double Prim where cancelK   _ _ = P.negate
-instance CommutativeK   Add Double Prim
-
-instance SemigroupK     Add Double Prim
-instance MonoidK        Add Double Prim
-instance GroupK         Add Double Prim
-
--- Double, multiplicative structure
 
 instance MagmaK         Mul Double Prim where binaryOpK _ _ = (P.*)
 instance NeutralK       Mul Double Prim where neutralK  _ _ = 1
 instance CancellativeK  Mul Double Prim where cancelK   _ _ = (1 P./)
-instance CommutativeK   Mul Double Prim
-
-instance SemigroupK     Mul Double Prim
-instance MonoidK        Mul Double Prim
-instance GroupK         Mul Double Prim
 
 -- Integers
 
@@ -152,59 +112,48 @@ type instance CancellativeS        Add        Integer = Prim
 type instance CommutativeS   (_ :: BinaryTag) Integer = Prim
 
 type instance SemigroupS     (_ :: BinaryTag) Integer = Prim
-type instance MonoidS        (_ :: BinaryTag) Integer = Prim
-type instance GroupS               Add        Integer = Prim
 
--- Additive
+type instance MonoidS t Integer =
+     Composite
+       [SemigroupK := SemigroupS t Integer,
+        NeutralK := NeutralS t Integer]
+
+type instance GroupS Add Integer =
+     Composite
+       [MonoidK := MonoidS Add Integer,
+        CancellativeK := CancellativeS Add Integer]
 
 instance MagmaK         Add Integer Prim where binaryOpK _ _ = (P.+)
 instance NeutralK       Add Integer Prim where neutralK  _ _ = 0
 instance CancellativeK  Add Integer Prim where cancelK   _ _ = P.negate
-instance CommutativeK   Add Integer Prim
-
-instance SemigroupK     Add Integer Prim
-instance MonoidK        Add Integer Prim
-instance GroupK         Add Integer Prim
-
--- Multiplicative
 
 instance MagmaK         Mul Integer Prim where binaryOpK _ _ = (P.*)
 instance NeutralK       Mul Integer Prim where neutralK  _ _ = 1
-instance CommutativeK   Mul Integer Prim
-
-instance SemigroupK     Mul Integer Prim
-instance MonoidK        Mul Integer Prim
 
 -- | Complex numbers with real/imag. parts represented by Double
+
 type ComplexD = Complex Double
 
 type instance MagmaS         (_ :: BinaryTag) ComplexD = Prim
 type instance NeutralS       (_ :: BinaryTag) ComplexD = Prim
 type instance CancellativeS  (_ :: BinaryTag) ComplexD = Prim
 type instance CommutativeS   (_ :: BinaryTag) ComplexD = Prim
-
 type instance SemigroupS     (_ :: BinaryTag) ComplexD = Prim
-type instance MonoidS        (_ :: BinaryTag) ComplexD = Prim
-type instance GroupS         (_ :: BinaryTag) ComplexD = Prim
 
--- ComplexD, additive structure
+type instance MonoidS t ComplexD =
+     Composite
+       [SemigroupK := SemigroupS t ComplexD
+       , NeutralK := NeutralS t ComplexD]
+
+type instance GroupS t ComplexD =
+     Composite
+       [MonoidK := MonoidS t ComplexD,
+        CancellativeK := CancellativeS t ComplexD]
 
 instance MagmaK         Add ComplexD Prim where binaryOpK _ _ = (P.+)
 instance NeutralK       Add ComplexD Prim where neutralK  _ _ = 0
 instance CancellativeK  Add ComplexD Prim where cancelK   _ _ = P.negate
-instance CommutativeK   Add ComplexD Prim
-
-instance SemigroupK     Add ComplexD Prim
-instance MonoidK        Add ComplexD Prim
-instance GroupK         Add ComplexD Prim
-
--- ComplexD, multiplicative structure
 
 instance MagmaK         Mul ComplexD Prim where binaryOpK _ _ = (P.*)
 instance NeutralK       Mul ComplexD Prim where neutralK  _ _ = 1
 instance CancellativeK  Mul ComplexD Prim where cancelK   _ _ = (1 P./)
-instance CommutativeK   Mul ComplexD Prim
-
-instance SemigroupK     Mul ComplexD Prim
-instance MonoidK        Mul ComplexD Prim
-instance GroupK         Mul ComplexD Prim
