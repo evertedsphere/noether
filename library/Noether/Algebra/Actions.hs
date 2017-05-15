cc058582734d3db3e05e706b2edfae36563354e0
Soham Chowdhury <chow.soham@gmail.com>
2017-05-15 13:48:23 +0530

    Actions, group actions

--- library/Noether/Algebra/Actions.hs
+++ library/Noether/Algebra/Actions.hs
@@ -1,143 +1,58 @@
-{-# LANGUAGE ConstraintKinds        #-}
-{-# LANGUAGE DataKinds              #-}
-{-# LANGUAGE DefaultSignatures      #-}
-{-# LANGUAGE FlexibleContexts       #-}
-{-# LANGUAGE FlexibleInstances      #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE GADTs                  #-}
-{-# LANGUAGE KindSignatures         #-}
-{-# LANGUAGE MultiParamTypeClasses  #-}
-{-# LANGUAGE RebindableSyntax       #-}
-{-# LANGUAGE ScopedTypeVariables    #-}
-{-# LANGUAGE TemplateHaskellQuotes  #-}
-{-# LANGUAGE TypeFamilies           #-}
-{-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE TypeInType             #-}
-{-# LANGUAGE UndecidableInstances   #-}
 {-# OPTIONS_GHC -fno-warn-unticked-promoted-constructors #-}
 
-module Algebra.Actions where
+module Noether.Algebra.Actions where
 
-import           Algebra.Basics
-import           Data.Kind (type (*))
-import           Data.Proxy
-import           Prelude        hiding (Monoid, negate, recip, (*), (+), (-),
-                                 (/), fromInteger)
-import qualified Prelude        as P
-import Data.Complex
+import           Data.Complex
 
--- I could see this turning into a (type|data) family
-data IsLinear     = YesLinear     | NotLinear
-data IsCompatible = YesCompatible | NotCompatible
+import           Noether.Lemmata.Prelude
+import           Noether.Lemmata.TypeFu
 
--- | Associative/semigroup actions
+import           Noether.Algebra.Single.Types
 
-class
-  ( Semigroup ao a
-  , Semigroup bo b
-  ) => LeftActs
-       (ao :: BinaryTag) (a :: *)
-       (bo :: BinaryTag) (b :: *)
-       (action :: BinaryTag)
-       (linear :: IsLinear)
-       (compatible :: IsCompatible)
-  | ao a bo b action -> linear compatible
-  where
+-- | Oh, Either...
+data Side = L | R
 
-  leftAct :: Proxy ao -> Proxy bo -> Proxy action -> a -> b -> b
+class ActsK (lr :: k) a b (s :: k') where
+  actK :: Proxy s -> Proxy lr -> a -> b -> b
 
-  default leftAct
-    :: (a ~ b, Semigroup action a)
-    => Proxy ao -> Proxy bo -> Proxy action -> a -> b -> b
-  leftAct _ _ = binaryOp
+type family ActsS (lr :: k) (a :: Type) (b :: Type) = (r :: k')
 
-class
-  ( Semigroup ao a
-  , Semigroup bo b
-  ) => RightActs
-       (ao :: BinaryTag) (a :: *)
-       (bo :: BinaryTag) (b :: *)
-       (action :: BinaryTag)
-       (linear :: IsLinear)
-       (compatible :: IsCompatible)
-  | ao a bo b action -> linear compatible
-  where
+type Acts lr a b = ActsK lr a b (ActsS lr a b)
 
-  rightAct :: Proxy ao -> Proxy bo -> Proxy action -> a -> b -> b
+type LeftActs  a b = Acts L a b
+type RightActs a b = Acts R a b
 
-  default rightAct
-    :: (a ~ b, Semigroup action a)
-    => Proxy ao -> Proxy bo -> Proxy action -> a -> b -> b
-  rightAct _ _ = binaryOp
+-- | A compatible action satisfies
+-- a `act` (a' `act` b) = (a `op` a') `act` b
+class (Acts lr a b, Semigroup op a) => CompatibleK (lr :: k1) op a b (s :: k2)
 
-type LeftLinear       ao a bo b t = LeftActs  ao a bo b t YesLinear NotCompatible
-type RightLinear      ao a bo b t = RightActs ao a bo b t YesLinear NotCompatible
+type family CompatibleS (lr :: k1) (op :: k2) (a :: Type) (b :: Type) = (r :: k3)
 
-type LeftCompatible   ao a bo b t = LeftActs  ao a bo b t NotLinear YesCompatible
-type RightCompatible  ao a bo b t = RightActs ao a bo b t NotLinear YesCompatible
+type Compatible lr op a b = CompatibleK lr op a b (CompatibleS lr op a b)
 
-instance (Ring Add m a) => LeftActs  Add a Add a m   YesLinear NotCompatible
-instance (Ring Add m a) => RightActs Add a Add a m   YesLinear NotCompatible
+-- | Group actions
 
-instance (Ring p Mul a) => LeftActs  Mul a p   a Mul NotLinear YesCompatible
-instance (Ring p Mul a) => RightActs Mul a p   a Mul NotLinear YesCompatible
+type family GSetS (lr :: k1) (op :: k2) (g :: Type) (b :: Type) = (r :: k3)
 
-scale :: Double -> Complex Double -> Complex Double
-scale d (a :+ b) = (d * a) :+ (d * b)
+class (Group op g, Compatible lr op g b) => GSetK lr op g b s
 
-instance LeftActs  Add Double Add (Complex Double) Mul YesLinear NotCompatible where
-  leftAct _ _ _ = scale
-instance RightActs Add Double Add (Complex Double) Mul YesLinear NotCompatible where
-  rightAct _ _ _ = scale
+type GSet lr op g b = GSetK lr op g b (GSetS lr op g b)
 
-instance LeftActs  Mul Double Add (Complex Double) Mul NotLinear YesCompatible where
-  leftAct _ _ _ = scale
-instance RightActs Mul Double Add (Complex Double) Mul NotLinear YesCompatible where
-  rightAct _ _ _ = scale
+type LeftGSet  op g b = GSet L op g b
+type RightGSet op g b = GSet R op g b
 
--- instance ( LeftActs Add k Add a m YesLinear NotCompatible
---          , LeftActs Add k Add b m YesLinear NotCompatible) =>
---          LeftActs Add k Add (a, b) m YesLinear NotCompatible where
---   leftAct p1 p2 p3 s (t, t') = (leftAct p1 p2 p3 s t, leftAct p1 p2 p3 s t')
+type family ActorLinearS (lr :: k1) (ao :: k2) (a :: Type) (bo :: k3) (b :: Type) = (r :: k3)
 
-instance ( LeftActs Add a Add b m YesLinear NotCompatible
-         , LeftActs Add a Add c m YesLinear NotCompatible
-         ) =>
-         LeftActs Add a Add (b, c) m YesLinear NotCompatible where
-  leftAct p1 p2 p3 s (t, t') = (leftAct p1 p2 p3 s t, leftAct p1 p2 p3 s t')
+class (Acts lr a b, Semigroup ao a, Semigroup bo b) =>
+      ActorLinearK lr ao a bo b s
 
-instance ( RightActs Add a Add b m YesLinear NotCompatible
-         , RightActs Add a Add c m YesLinear NotCompatible
-         ) =>
-         RightActs Add a Add (b, c) m YesLinear NotCompatible where
-  rightAct p1 p2 p3 s (t, t') = (rightAct p1 p2 p3 s t, rightAct p1 p2 p3 s t')
+type ActorLinear lr ao a bo b = ActorLinearK lr ao a bo b (ActorLinearS lr ao a bo b)
 
-instance ( LeftActs Mul a p b Mul NotLinear YesCompatible
-         , LeftActs Mul a p c Mul NotLinear YesCompatible
-         ) =>
-         LeftActs Mul a p (b, c) Mul NotLinear YesCompatible where
-  leftAct p1 p2 p3 s (t, t') = (leftAct p1 p2 p3 s t, leftAct p1 p2 p3 s t')
+type family ActeeLinearS (lr :: k1) (ao :: k2) (a :: Type) (bo :: k3) (b :: Type) = (r :: k3)
 
-instance ( RightActs Mul a p b Mul NotLinear YesCompatible
-         , RightActs Mul a p c Mul NotLinear YesCompatible
-         ) =>
-         RightActs Mul a p (b, c) Mul NotLinear YesCompatible where
-  rightAct p1 p2 p3 s (t, t') = (rightAct p1 p2 p3 s t, rightAct p1 p2 p3 s t')
+class (Acts lr a b, Semigroup ao a, Semigroup bo b) =>
+      ActeeLinearK lr ao a bo b s
 
-abelianIntegerAction
-  :: (AbelianGroup op a)
-  => Proxy add -> Proxy op -> Proxy mul -> Integer -> a -> a
-abelianIntegerAction _ opProxy _ 0 _ = neutral opProxy
-abelianIntegerAction _ _ _ 1 a = a
-abelianIntegerAction addProxy opProxy mulProxy n a =
-  binaryOp opProxy a (abelianIntegerAction addProxy opProxy mulProxy (n P.- 1) a)
+type ActeeLinear lr ao a bo b = ActeeLinearK lr ao a bo b (ActeeLinearS lr ao a bo b)
 
-instance (AbelianGroup op a) => LeftActs  Add Integer op a Mul YesLinear NotCompatible where
-  leftAct  = abelianIntegerAction
-instance (AbelianGroup op a) => RightActs Add Integer op a Mul YesLinear NotCompatible where
-  rightAct = abelianIntegerAction
-
-instance (AbelianGroup op a) => LeftActs  Mul Integer op a Mul NotLinear YesCompatible where
-  leftAct  = abelianIntegerAction
-instance (AbelianGroup op a) => RightActs Mul Integer op a Mul NotLinear YesCompatible where
-  rightAct = abelianIntegerAction
+type LinearActs lr ao a bo b = (ActorLinear lr ao a bo b, ActeeLinear lr ao a bo b)
