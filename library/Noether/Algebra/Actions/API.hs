ae033704cf87f9e48bfed4991f344518cb66acee
Soham Chowdhury <chow.soham@gmail.com>
2017-05-20 15:52:53 +0530

    Demo local instance functionality

--- library/Noether/Algebra/Actions/API.hs
+++ library/Noether/Algebra/Actions/API.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications    #-}
 module Noether.Algebra.Actions.API
   ( leftAct
   , rightAct
+  , leftActK
+  , rightActK
   ) where
 
 import           Noether.Lemmata.TypeFu
@@ -13,20 +16,15 @@ import           Noether.Algebra.Actions.Compatible
 import           Noether.Algebra.Actions.Linearity
 
 leftActK
-  :: forall a b s op. ActsK 'L op a b s
-  => Proxy op -> Proxy s -> a -> b -> b
-leftActK opP sP = actK opP sP (Proxy :: Proxy 'L)
+  :: forall op s a b. ActsK 'L op a b s => a -> b -> b
+leftActK = actK (Proxy :: Proxy op) (Proxy :: Proxy s) (Proxy :: Proxy 'L)
 
 rightActK
-  :: forall a b s op. ActsK 'R op a b s
-  => Proxy op -> Proxy s -> a -> b -> b
-rightActK opP sP = actK opP sP (Proxy :: Proxy 'R)
+  :: forall op s a b. ActsK 'R op a b s => a -> b -> b
+rightActK = actK (Proxy :: Proxy op) (Proxy :: Proxy s) (Proxy :: Proxy 'R)
 
 leftAct :: forall op a b. LeftActs op a b => a -> b -> b
-leftAct = leftActK (Proxy :: Proxy op) (Proxy :: Proxy (ActsS 'L op a b))
+leftAct = leftActK @op @(ActsS 'L op a b)
 
 rightAct :: forall op a b. RightActs op a b => a -> b -> b
-rightAct = rightActK (Proxy :: Proxy op) (Proxy :: Proxy (ActsS 'R op a b))
-
--- a :: RightActs Double a => a -> a
--- a = rightAct (3 :: Double)
+rightAct = rightActK @op @(ActsS 'R op a b)
--- library/Noether/Algebra/Linear/API.hs
+++ library/Noether/Algebra/Linear/API.hs
@@ -22,10 +22,15 @@ r %< v = leftAct @Mul r v
 (>%) :: RightModule' r v => v -> r -> v
 v >% r = rightAct @Mul r v
 
+-- | Locally use the action induced by the multiplicative magma structure on the
+-- ring, whatever structure the user may have chosen to use globally.
+(>%%) :: forall r. Ring Add Mul r => r -> r -> r
+a >%% b = rightActK @Mul @(DeriveActs_Magma Mul r) a b
+
 -- | Linear interpolation.
 -- >>> lerp λ v w = λv + (1 - λ)w
 lerp
-  :: ( LeftModule' r v
-     , RightModule' r v
-     ) => r -> v -> v -> v
+  :: ( LeftModule' r r
+     , RightModule' r r
+     ) => r -> r -> r -> r
 lerp lambda v w = lambda %< v + w >% (one - lambda)
